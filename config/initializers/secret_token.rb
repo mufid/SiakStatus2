e72f4c2b35ef811431280072ca8190cfb6cc764b
Mufid Afif <muhammad@icehousecorp.com>
2013-08-06 12:43:14 +0700

    Initial move to Rails 4

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SiakNew::Application.config.secret_key_base = '4bc0e1ff7fd69da92d363312d667f681d9d11824b83041c7f32225ad2f62dc839526e63b2a608ac96c14e636e54a3623d004d46305c3b70c65f77eaee71f2a9b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SiakNew::Application.config.session_store :cookie_store, key: '_siak-new_session'
