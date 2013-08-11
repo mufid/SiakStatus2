59bc41a8a3246b07a963cbca41b9224dcf00344a
Mufid Afif <mufid@idmail.or.id>
2013-08-11 17:42:02 +0700

    Final heroku push

--- app/controllers/siak_statuses_controller.rb
+++ app/controllers/siak_statuses_controller.rb
@@ -3,9 +3,19 @@ class SiakStatusesController < ApplicationController
   # GET /stasiuns.json
   def index
     @statuses = SiakStatus.order("created_at DESC").page(params[:page]).per(50)
-    @data_json = SiakStatus.limit(50).map { |x| [(x.id % 5 == 0) || true ? x.created_at.strftime("%H:%M:%S") : "" , x.ping_ms] }
+    @data_json = SiakStatus.limit(50).map { |x| [x.created_at.strftime("%H:%M:%S"), x.ping_ms] }
     @data_json.unshift(["x", "Ping (dalam ms)"])
     
+    ping_ms = SiakStatus.order("created_at DESC").first.ping_ms
+
+    @status_string =
+      if ping_ms < 5000
+        "Yeee.. SIAK baik-baik saja!"
+      elsif ping_ms < 15000
+        "Sepertinya SIAK mengalami masalah.."
+      else
+        "Auch.. SIAK tidak bisa diakses!"
+      end
 
     respond_to do |format|
       format.html # index.html.erb
--- app/models/siak_status.rb
+++ app/models/siak_status.rb
@@ -1,2 +1,21 @@
 class SiakStatus < ActiveRecord::Base
+  def self.status_string
+    case status_sym
+    when :ok
+      "Siak baik-baik saja"
+    when :warn
+      "Kamu mungkin akan mengalami masalah kecepatan"
+    when :fail
+      "Ooopppssss.. sepertinya SIAK mengalami masalah"
+    end
+  end
+  def self.status_sym
+    if @ping_ms < 5000
+      :ok
+    elsif @ping_ms < 10000
+      :warn
+    else
+      :fail
+    end
+  end
 end
