6eb2f55677ad19ef98db2921f5e7a5f89e0291b1
Mufid Afif <mufid@idmail.or.id>
2013-08-11 17:19:50 +0700

    Working chart

--- app/components/status_retrieval.rb
+++ app/components/status_retrieval.rb
@@ -3,6 +3,7 @@ class StatusRetrieval
   # Constants
   URI_LOGIN = "https://academic.ui.ac.id/main/Authentication/Index"
   URI_TARGET = "https://academic.ui.ac.id/main/CoursePlan/CoursePlanEdit"
+  RTO_TIME = 30000
   
   def initialize username, password
     @username = username
@@ -11,11 +12,15 @@ class StatusRetrieval
   def perform
     beginning_time = Time.now
     
-    ck = get_cookie_login
-    retrieve_uri ck
+    begin 
+      ck = get_cookie_login
+      retrieve_uri ck 
+    rescue
+      rto = true
+    end
     
     record = SiakStatus.new
-    record.ping_ms = (Time.now - beginning_time) * 1000
+    record.ping_ms = rto ? RTO_TIME : (Time.now - beginning_time) * 1000
     record
   end
   def get_cookie_login
--- app/controllers/siak_statuses_controller.rb
+++ app/controllers/siak_statuses_controller.rb
@@ -2,8 +2,10 @@ class SiakStatusesController < ApplicationController
   # GET /stasiuns
   # GET /stasiuns.json
   def index
-
     @statuses = SiakStatus.order("created_at DESC").page(params[:page]).per(50)
+    @data_json = SiakStatus.limit(50).map { |x| [(x.id % 5 == 0) || true ? x.created_at.strftime("%H:%M:%S") : "" , x.ping_ms] }
+    @data_json.unshift(["x", "Ping (dalam ms)"])
+    
 
     respond_to do |format|
       format.html # index.html.erb
